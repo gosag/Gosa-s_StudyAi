455beb2495c5dfb09255be6ef0d3e345007c8383
Gosa Girma <gosagirma441@gmail.com>
2026-02-27 21:15:22 +0300

    use morgan library in your app

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import authRouter from "./routes/auth";
 import errorHandler from "./middleware/error";
-
+import morgan from "morgan"
 const app=express()
 //parsers
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
+//logger
+app.use(morgan("dev"))
 //routes
 app.use(authRouter)
 //error handler
