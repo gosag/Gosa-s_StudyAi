99ddb1c7fb4d64cbcd9be9af43521fa7d974a323
Gosa Girma <gosagirma441@gmail.com>
2026-03-01 15:40:13 +0300

    add side bar to the app and make it responsive

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
-import SignUp from "./components/signUp/signUp"
-import Login from "./components/Login/login"
+import SignUp from "./components/signUp"
+import Login from "./components/login"
 import MainOutLet from "./components/outlet/MainOutlet"
+import Sidebar from "./components/sideBar"
 import { Routes, Route } from "react-router-dom"
-
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainOutLet />}>
-        <Route index element={<SignUp/>} />
+        <Route index element={<Sidebar/>} />
+        <Route path="signUp" element={<SignUp/>}/>
         <Route path="login" element={<Login />} />
       </Route>
     </Routes>
