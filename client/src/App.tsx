931532deb73d57363add356e2ae7c768b635241d
Gosa Girma <gosagirma441@gmail.com>
2026-03-02 15:16:07 +0300

    fix: update card title and content in the main route, enhance button styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,22 @@ import Login from "./components/login"
 import MainOutLet from "./components/outlet/MainOutlet"
 import { Routes, Route } from "react-router-dom"
 import { Button } from "./components/ui/button"
-import { Card, CardContent, CardFooter } from "./components/ui/card"
-import { CardHeader } from "./components/ui/card"
-import { CardTitle } from "./components/ui/card"
+import { Card, CardContent, CardFooter ,CardHeader, CardTitle } from "./components/ui/card"
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainOutLet/>}>
         <Route index element={<Card className="w-87.5">
         <CardHeader>
-          <CardTitle>EchoLearn</CardTitle>
+          <CardTitle>Your AI Study Dashboard</CardTitle>
         </CardHeader>
 
         <CardContent>
-          This is your first shadcn Card component.
+          Summarize materials, generate quizzes, and review flashcards smarter.
         </CardContent>
 
         <CardFooter className="flex justify-end">
-          <Button className="bg-blue-500">Start Studying</Button>
+          <Button className="bg-blue-500 hover:bg-blue-600">Start Studying</Button>
         </CardFooter>
       </Card>}/>
         <Route path="library" element={<Card className="w-87.5">
