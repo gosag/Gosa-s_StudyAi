a5a6b4821bf4e349f7b0d98f25972448cc35e276
Gosa Girma <gosagirma441@gmail.com>
2026-03-02 13:02:24 +0300

    feat: add UI components and styling enhancements
    - Added new dependencies for UI components: class-variance-authority, clsx, lucide-react, radix-ui, and tailwind-merge.
    - Implemented a Card component with CardHeader, CardContent, CardFooter, and Button components for better UI structure.
    - Updated App component to utilize new Card and Button components for the home and library routes.
    - Enhanced global styles with Tailwind CSS and custom theming using CSS variables.
    - Configured TypeScript paths for easier imports and updated Vite configuration for aliasing.
    - Created utility functions for class name management.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,40 @@ import SignUp from "./components/signUp"
 import Login from "./components/login"
 import MainOutLet from "./components/outlet/MainOutlet"
 import { Routes, Route } from "react-router-dom"
+import { Button } from "./components/ui/button"
+import { Card, CardContent, CardFooter } from "./components/ui/card"
+import { CardHeader } from "./components/ui/card"
+import { CardTitle } from "./components/ui/card"
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainOutLet/>}>
-        <Route index element={<h1>Home</h1>}/>
-        <Route path="library" element={<h1>Library</h1>}/>
+        <Route index element={<Card className="w-87.5">
+        <CardHeader>
+          <CardTitle>EchoLearn</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          This is your first shadcn Card component.
+        </CardContent>
+
+        <CardFooter className="flex justify-end">
+          <Button className="bg-blue-500">Start Studying</Button>
+        </CardFooter>
+      </Card>}/>
+        <Route path="library" element={<Card className="w-87.5">
+        <CardHeader>
+          <CardTitle>EchoLearn</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          This is your first shadcn Card component.
+        </CardContent>
+
+        <CardFooter className="flex justify-end">
+          <Button>Start Studying</Button>
+        </CardFooter>
+      </Card>}/>
         <Route path="flashcards" element={<h1>flashcards</h1>}/>
         <Route path="settings" element={<>settings</>}/>
         <Route path="signUp" element={<SignUp/>}/>
