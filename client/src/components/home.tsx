984eba3be424d0aae0a5f758e970e7af85784284
Gosa Girma <gosagirma441@gmail.com>
2026-03-04 08:18:55 +0300

    feat: enhance file upload functionality with multer and add pdf service

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -47,19 +47,18 @@ function Home() {
             if(e.target.files){
               setFile(e.target.files[0])
             }
-          }} type="file" accept="application/pdf"  className="w-8 h-8 opacity-0 z-20 bg-transparent text-black absolute bottom-3 left-3 p-0  cursor-pointer"/>
+          }} type="file" name="pdf" accept="application/pdf"  className="w-8 h-8 opacity-0 z-20 bg-transparent text-black absolute bottom-3 left-3 p-0  cursor-pointer"/>
           <Textarea
             placeholder="Paste link or type text..." 
             className="pl-12 py-3 min-h-11 max-h-40 resize-none overflow-y-auto rounded-2xl"
           />
-          <Button onClick={handleUpload} className={file?"bg-green-400 text-black relative ml-2 h-11 w-11 p-0 rounded-full cursor-pointer hover:bg-green-500 active:scale-100 hover:scale-105 transition-all duration-200 shrink-0 flex items-center justify-center": "bg-gray-200 text-black relative ml-2 h-11 w-11 p-0 rounded-full cursor-pointer hover:bg-gray-300 hover:scale-105 transition-all duration-200 shrink-0 flex items-center justify-center"}>
+          <Button onClick={handleUpload} className={`text-black relative ml-2 h-11 w-11 p-0 rounded-full cursor-pointer active:scale-100 hover:scale-105 transition-all duration-200 shrink-0 flex items-center justify-center ${file?"bg-green-400 hover:bg-green-500":"bg-gray-200 hover:bg-gray-300"}`}>
               <Send className="w-5 h-5 ml-0.5" /> 
           </Button>
         </CardFooter>
       </Card>
       <div className="transition duration-300 hover:-translate-y-2 hover:rotate-01 hover:scale-103">
         <StudyStack />
-       
       </div>
       </div>
     )}
--- server/middleware/uploads.ts
+++ server/middleware/uploads.ts
@@ -1,7 +1,13 @@
 import express from "express"
 import multer from "multer"
 const uploadRoute=express.Router();
-const upload=multer({dest:"/uploads"});
+const upload=multer({dest:"uploads/",
+    limits:{fileSize:10*1024*1024},
+    fileFilter:(req,file,cb)=>{
+        if(file.mimetype!=="application/pdf"){
+            return cb(new Error("Only PDF files are allowed"))
+        }        cb(null,false)      
+}});
 uploadRoute.post("/api/uploads",upload.single("pdf"),(req,res)=>{
     if(!req.file){
         return res.status(400).json({error:"No file uploaded"})
