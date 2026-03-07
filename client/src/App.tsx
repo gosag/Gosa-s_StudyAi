d6bfe58e057f58d45a5138fb27d08c0ae9dc0229
Gosa Girma <gosagirma441@gmail.com>
2026-03-07 22:04:43 +0300

    feat: implement Library component and update routing in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import Login from "./components/login"
 import MainOutLet from "./components/outlet/MainOutlet"
 import { Routes, Route } from "react-router-dom"
 import Home from "./components/home"
+import Library from "./components/library"
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainOutLet/>}>
         <Route index element={<Home/>}/>
-        <Route path="library" element={<h1>library</h1>}/>
+        <Route path="library" element={<Library/>}/>
         <Route path="flashcards" element={<h1>flashcards</h1>}/>
         <Route path="settings" element={<>settings</>}/>
         <Route path="signUp" element={<SignUp/>}/>
---
+++ client/src/components/library.tsx
@@ -0,0 +1,22 @@
+import {Card, CardFooter, CardHeader, CardContent, CardTitle} from "../components/ui/card"
+import {Button} from "../components/ui/button"
+import {FileText} from "lucide-react"
+function Library(){
+    return(
+        <>
+            <Card className="w-60">
+                <CardHeader>
+                    <CardTitle><FileText size={20} className="inline"/> Data Sturucture & Al</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    
+                </CardContent>
+                <CardFooter className="flex justify-between mx-2">
+                    <Button className="bg-green-500 shadow-2xl hover:bg-green-600 hover:scale-105 transition-all duration-200">Continues</Button>
+                    <Button className="bg-red-500 hover:bg-red-600 hover:scale-105 transition-all duration-200">Delete</Button>
+                </CardFooter>
+            </Card>
+        </>
+    )
+}
+export default Library
