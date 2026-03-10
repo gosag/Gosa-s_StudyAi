606a1b6aed16a9f13630059e3da24c9e22294ad7
Gosa Girma <gosagirma441@gmail.com>
2026-03-10 13:39:33 +0300

    feat: enhance library component; improve material rendering and add delete functionality with updated button styles

--- client/src/components/library.tsx
+++ client/src/components/library.tsx
@@ -1,4 +1,5 @@
 import {Card, CardFooter, CardHeader, CardContent, CardTitle} from "../components/ui/card"
+import { Trash2 } from "lucide-react";
 import {Button} from "../components/ui/button"
 import {FileText} from "lucide-react"
 import { useState,useEffect} from "react"
@@ -25,6 +26,9 @@ useEffect(()=>{
                 }
             })
             const data = await res.json();
+            if(!res.ok){
+                throw new Error(data.error || "Failed to fetch materials")
+            }
             setMaterials(data.materials || []);
         } catch(error){
             console.log(error)
@@ -36,18 +40,19 @@ useEffect(()=>{
         <>  
             <div>
             {materials.length>0?(
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {materials.map((material)=>(
+                <div className="flex flex-wrap gap-6 justify-center">
+                {materials.map((material)=>(
                 <Card key={material._id} className="w-60">
-                <CardHeader>
+                <CardHeader className="grid grid-cols-2">
                     <CardTitle><FileText size={20} className="inline wrap-break-word"/>{material.title}</CardTitle>
+                    <Trash2 size={28} className="ml-auto cursor-pointer text-red-500 hover:text-red-600" />
                 </CardHeader>
                 <CardContent>
                     <p className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
                 </CardContent>
                 <CardFooter className="flex justify-between mx-2">
                     <Button className="bg-green-500 shadow-2xl hover:bg-green-600 hover:scale-105 transition-all duration-200">Continue</Button>
-                    <Button className="bg-red-500 hover:bg-red-600 hover:scale-105 transition-all duration-200">Delete</Button>
+                    <Button className="bg-blue-500 hover:bg-blue-600 hover:scale-105 transition-all duration-200">Quiz</Button>
                 </CardFooter>
             </Card>))}</div>):<div>fetching data</div>}
             </div>
