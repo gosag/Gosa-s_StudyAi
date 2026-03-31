c103d65c35f0100c2fc8f67cca703875677569b1
Gosa Girma <gosagirma441@gmail.com>
2026-03-31 23:22:48 +0300

    feat: disable submit button during loading state in login and sign-up forms

--- server/src/middleware/rateLimitor.ts
+++ server/src/middleware/rateLimitor.ts
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 export const rateLimiter=rateLimit({
-    windowMs:15*60*1000,
+    windowMs:5*60*1000,
     max:3,
-    message:"Too many Request try after 15 minutes!",
+    message:"Too many Request try after 5 minutes!",
     standardHeaders:true,
     legacyHeaders:false
 })
