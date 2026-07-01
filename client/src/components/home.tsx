bf69e90e8f7da5558943140acbd35f1b79a2fa0f
Gosa Girma <gosagirma441@gmail.com>
2026-07-01 11:03:53 +0300

    fix: fix theme issue that changes the theme upon clicking the settings

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -38,14 +38,21 @@ function Home() {
     return false
   }
 }
+
  const [theme] = useState<"light" | "dark">(() => {
     if (typeof window !== "undefined") {
       const savedTheme = localStorage.getItem("theme");
       if (savedTheme === "dark" || savedTheme === "light") return savedTheme;
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) return "dark";
+      /* if (window.matchMedia("(prefers-color-scheme: dark)").matches) return "dark"; */
     }
     return "light";
   });
+useEffect(() => { 
+    const root = window.document.documentElement;
+    root.classList.remove("light", "dark");
+    root.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   useEffect(() => { 
     const root = window.document.documentElement;
